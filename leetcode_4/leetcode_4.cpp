ef0f1f65401c7ce7d434c92a72d339f1f76134ce
zeiland <963531180@qq.com>
2019-06-02 16:41:55 +0800

    [add] leetcode_4

--- leetcode_4/leetcode_4.cpp
+++ leetcode_4/leetcode_4.cpp
@@ -7,7 +7,7 @@ using namespace std;
 class Solution {
 public:
 
-int find_kth(const vector<int>& nums1, int begin1, const vector<int>& nums2, int begin2, int k)
+int findKth(const vector<int>& nums1, int begin1, const vector<int>& nums2, int begin2, int k)
 {
 	if (begin1 == nums1.size()){
 		return nums2[begin2 + k - 1];
@@ -21,9 +21,9 @@ int find_kth(const vector<int>& nums1, int begin1, const vector<int>& nums2, int
 	int mid = min(k / 2, static_cast<int>(nums1.size()));
 	mid = min(static_cast<int>(nums2.size()), mid);
 	if (nums1[begin1 + mid - 1] < nums2[begin2 + mid - 1]){
-		return find_kth(nums1, begin1 + mid, nums2, begin2, k - mid);
+		return findKth(nums1, begin1 + mid, nums2, begin2, k - mid);
     } else {
-		return find_kth(nums1, begin1, nums2, begin2 + mid, k - mid);
+		return findKth(nums1, begin1, nums2, begin2 + mid, k - mid);
     }
 }
 
@@ -33,9 +33,9 @@ double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
 	int m = nums1.size();
 	int n = nums2.size();
 	if ((m + n) % 2 == 0) {
-		return (static_cast<double>(find_kth(nums1, 0, nums2, 0, (m + n) / 2)) + find_kth(nums1, 0, nums2, 0, (m + n) / 2 + 1)) / 2;
+		return (static_cast<double>(findKth(nums1, 0, nums2, 0, (m + n) / 2)) + findKth(nums1, 0, nums2, 0, (m + n) / 2 + 1)) / 2;
     } else {
-		return static_cast<double>(find_kth(nums1, 0, nums2, 0, (m + n) / 2 + 1));
+		return static_cast<double>(findKth(nums1, 0, nums2, 0, (m + n) / 2 + 1));
     }
 }
 
