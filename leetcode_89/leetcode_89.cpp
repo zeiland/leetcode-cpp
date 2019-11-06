5b405fc94d43bae70f1a3212048d5c0af1101b68
zeiland <963531180@qq.com>
2019-11-06 16:27:16 +0800

    add leetcode_89

---
+++ leetcode_89/leetcode_89.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+     vector<int> grayCode(int n) {
+         vector<int> result;
+         for(int i=0;i<1<<n;i++)
+         {
+             result.push_back(i^i>>1);
+         }
+         return result;
+     }
+};
