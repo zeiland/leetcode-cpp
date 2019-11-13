437aaee4bba4099d5de07c18073b66de9550cecc
zeiland <963531180@qq.com>
2019-11-13 19:30:24 +0800

    add leetcode_954

---
+++ leetcode_954/leetcode_954.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+class Solution 
+{
+public:
+    bool canReorderDoubled(vector<int>& A) {
+        map<int, int> A_dict;
+        vector<int> keys;
+        for (auto a : A){
+             A_dict[a]++;
+        }
+        for (auto& a: A_dict) {
+            keys.push_back(a.first);
+        }
+        sort(keys.begin(), keys.end(), [](int a, int b){
+            return abs(a) < abs(b);
+        });
+        for (auto k : keys){
+            if (A_dict[k] > A_dict[k*2]) {
+                return false;
+            }
+            A_dict[2*k] -= A_dict[k];
+        }
+        return true;
+    }
+};
