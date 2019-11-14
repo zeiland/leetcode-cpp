2f88b4b15d2644d2d52fed37db99434cd443349c
zeiland <963531180@qq.com>
2019-11-14 17:47:28 +0800

    add leetcode_1223

---
+++ leetcode_1223/leetcode_1223.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int dieSimulator(int n, vector<int>& rollMax) {
+        long p = 1e9 + 7;
+        vector<vector<long>> dp(n+1, vector<long>(6, 0));
+        vector<long> sum(n+1, 0);
+        sum[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < 6; ++j)  {
+                for (int k = 1; k <= rollMax[j] && i - k >= 0; ++k)
+                    dp[i][j] = (dp[i][j] + sum[i-k] - dp[i-k][j] + p) % p;
+                sum[i] = (sum[i] + dp[i][j]) % p;
+            }                
+        }
+        
+        return sum[n];
+    }
+};
