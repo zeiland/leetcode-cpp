e428c5b09ecedb4821fbb37cf29874c131704b2d
zeiland <963531180@qq.com>
2020-09-22 01:05:48 +0800

    format leetcode_1.cpp

--- leetcode_1/leetcode_1.cpp
+++ leetcode_1/leetcode_1.cpp
@@ -2,18 +2,23 @@
 #include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> m;
-        for(int i=0;i<nums.size();++i){
-            m[nums[i]]=i;
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            m[nums[i]] = i;
         }
         vector<int> ret;
-        for(int i=0;i<nums.size();++i){
-            if(m.find(target-nums[i])!=m.end()&&m[target-nums[i]]!=i){
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (m.find(target - nums[i]) != m.end() && m[target - nums[i]] != i)
+            {
                 ret.push_back(i);
-                ret.push_back(m[target-nums[i]]);
+                ret.push_back(m[target - nums[i]]);
                 return ret;
             }
         }
